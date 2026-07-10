3c7e8214a21706502b66c1426f5659e53855191c
Praveen Singh <praveen969500@gmail.com>
2026-07-10 16:47:27 +0530

    fix the issue

--- Frontend/frontend/src/api/axios.js
+++ Frontend/frontend/src/api/axios.js
@@ -16,13 +16,17 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
-      window.location.href = '/login';
+   if (
+      error.response?.status === 401 &&
+      !error.config.url.includes("/auth/login")
+    ) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
     }
     return Promise.reject(error);
   }
 );
 
 export default api;
+
